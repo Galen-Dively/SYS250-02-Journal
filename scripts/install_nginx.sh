bbd24e8781d9ddc375bc70dfe96af3541bd0437e
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2024-11-08 10:55:22 -0500

    Added

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Install nginx
-sudo dnf install nginx -y
+# Install nginx & nodejs
+sudo dnf install nginx nodejs -y
+sudo systemctl start nginx
 sudo systemctl enable --now nginx
-echo "Nginx has been installed"
+echo "Nginx has been installed and Node"
 
 # Allow HTTP Traffic
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --reload
 echo "HTTP has been allowed"
+
+# Installing MariaDB
+sudo dnf install mariadb-server -y
+echo "Enabling MariaDB"
+sudo systemctl start mariadb
+sudo systemctl enable --now mariadb
+
+
+echo "Everything is up, run myqsl_secure_installation as super user to proceed"
+
